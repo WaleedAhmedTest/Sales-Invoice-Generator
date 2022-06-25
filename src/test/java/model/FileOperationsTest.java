6280bf65467bc2e8cd431d74b0f3850dbeba7710
Amr Momtaz <waleedahmed10851085@gmail.com>
2022-06-25 16:22:24 +0200

    addinng tests

--- src/test/java/model/FileOperationsTest.java
+++ src/test/java/model/FileOperationsTest.java
@@ -1,20 +1,16 @@
 package model;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class FileOperationsTest {
 
-    @org.junit.jupiter.api.Test
-    void readFile() throws FileNotFoundException {
-        FileOperations fileOperations = new FileOperations();
-        ArrayList<InvoiceHeader> invoiceHeaders = fileOperations.readFile();
-        for (InvoiceHeader invoiceHeader : invoiceHeaders)
-            System.out.println(invoiceHeader);
+    @Test
+    void readFile() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void writeFile() {
-
     }
 }
